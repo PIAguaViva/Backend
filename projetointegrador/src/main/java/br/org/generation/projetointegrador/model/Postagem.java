0bd6c4b3bd6bdd37755b324461750bc4bd25e2b5
Scarlatt Luz <contatoscarlatt@gmail.com>
2022-01-27 10:35:34 -0300

    Criação da Model Postagem, seus atributos e relacionamentos

---
+++ projetointegrador/src/main/java/br/org/generation/projetointegrador/model/Postagem.java
@@ -0,0 +1,104 @@
+package br.org.generation.projetointegrador.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_postagem")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="O campo é obrigatório!")
+	@Size(min = 5, max = 100, message="Este campo deve conter no mínimo 5 e máximo 100 caracteres.")
+	private String titulo;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@NotBlank(message="O campo é obrigatório!")
+	@Size(max = 255, message="Este campo deve conter no máximo 255 caracteres.")
+	private String regiao;
+	
+	@NotBlank(message="O campo é obrigatório!")
+	@Size(min = 10, max = 1000, message="Este campo deve conter no mínimo 5 e máximo 100 caracteres.")
+	private String texto;
+	
+	@Size(max = 1000, message="Este campo deve conter no máximo 1000 caracteres.")
+	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+}
--- projetointegrador/src/main/java/br/org/generation/projetointegrador/model/Tema.java
+++ projetointegrador/src/main/java/br/org/generation/projetointegrador/model/Tema.java
@@ -1,12 +1,18 @@
 package br.org.generation.projetointegrador.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_tema")
@@ -25,6 +31,9 @@ public class Tema {
 	@Size(max=255,min=10,message="O atributo descrição deve conter no mínimo 10 e no máximo 255 caracteres")
 	private String descricao;
 
+	@OneToMany(mappedBy="tema", cascade=CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
 
 	public Long getId() {
 		return id;
@@ -53,6 +62,16 @@ public class Tema {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 
 }
